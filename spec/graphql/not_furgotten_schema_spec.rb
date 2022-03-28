85fb47496d25cc39bf547b53fad52ef78403737f
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-28 11:12:09 -0500

    Test- edit test to not have.strip for described_class.to_definition

--- spec/graphql/not_furgotten_schema_spec.rb
+++ spec/graphql/not_furgotten_schema_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe NotFurgottenSchema do
   it 'matches the dumped schema (rails graphql:schema:dump)' do
     aggregate_failures do
-      #without rstrip n line 7 it fails because of whitespace
-      expect(described_class.to_definition.rstrip).to eq(File.read(Rails.root.join('schema.graphql')).rstrip)
+      
+      expect(described_class.to_definition).to eq(File.read(Rails.root.join('schema.graphql')))
       expect(described_class.to_json).to eq(File.read(Rails.root.join('schema.json')).rstrip)
-    end 
-  end 
-end 
+    end
+  end
+end
