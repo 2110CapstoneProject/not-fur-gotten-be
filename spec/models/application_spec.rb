3f12f213a3707e201714bc2af36484536344c447
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-29 13:20:31 -0500

    Test: add tests to validate presence of all application attributes

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -2,5 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Application, type: :model do
   it {should belong_to(:pet)}
-
+  it {should validate_presence_of :name}
+  it {should validate_presence_of :email}
+  it {should validate_presence_of :description}
+  it {should validate_presence_of :pet_id}
 end
