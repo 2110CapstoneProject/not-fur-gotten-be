3cc9c90b336cbff99ea57bac72456eec7120a9b6
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-31 13:15:05 -0500

    Add create pet mutation with errors and spec file

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,12 +2,5 @@ module Types
   class MutationType < Types::BaseObject
     field :create_application, mutation: Mutations::CreateApplication
     field :create_pet, mutation: Mutations::CreatePet
-
-    # TODO: remove me
-    # field :test_field, String, null: false,
-    #   description: "An example field added by the generator"
-    # def test_field
-    #   "Hello World"
-    # end
   end
 end
