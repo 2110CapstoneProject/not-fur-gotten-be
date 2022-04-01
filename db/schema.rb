687348fdd184a74e08c62425f59cab5cf06ec6e6
Karan Mehta <karanm645@gmail.com>
2022-03-31 19:10:07 -0500

    Create image column to pets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_26_215204) do
+ActiveRecord::Schema.define(version: 2022_04_01_000856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2022_03_26_215204) do
     t.string "owner_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   add_foreign_key "applications", "pets"
