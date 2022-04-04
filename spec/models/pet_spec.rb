a5187a19b1760ba67c2f6af404bcecf8f533c2ee
Karan Mehta <karanm645@gmail.com>
2022-04-04 16:26:37 -0500

    Add age validation greater than 0

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Pet, type: :model do
-  it {should have_many(:applications)}
+  describe 'validations' do 
+    it {should have_many(:applications)}
 
-  it {should validate_presence_of :name}
-  it {should validate_presence_of :age}
-  it {should validate_presence_of :description}
-  it {should validate_presence_of :species}
-  it {should validate_presence_of :gender}
-  it {should validate_presence_of :owner_email}
-  it {should validate_presence_of :owner_name}
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :age}
+    it {should validate_presence_of :description}
+    it {should validate_presence_of :species}
+    it {should validate_presence_of :gender}
+    it {should validate_presence_of :owner_email}
+    it {should validate_presence_of :owner_name}
+  end 
 end
